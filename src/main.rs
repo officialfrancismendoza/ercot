2de3f2c6e18ddc5aa4bf62f31d199991b75de438
Francis Mendoza <francissamuelmendoza7@gmail.com>
2023-03-22 01:19:07 -0700

    Store rudimentary website HTML in main method (#1)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
 fn main() {
-    println!("Hello, world!");
+    let response = reqwest::blocking::get(
+        https://www.ercot.com/content/cdr/html/20230322_dam_spp.html
+    )
+    .unwrap()
+    .text()
+    .unwrap();
 }
